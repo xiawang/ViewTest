aef0bff02f9c9d93e288a0f0f6185412e824a469
王小天 <wxt1994@gmail.com>
2014-09-10 23:12:28 -0700

    Initial Commit

---
+++ ViewTestTests/ViewTestTests.m
@@ -0,0 +1,34 @@
+//
+//  ViewTestTests.m
+//  ViewTestTests
+//
+//  Created by 王小天 on 14-9-10.
+//  Copyright (c) 2014年 Xiaotian Wang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ViewTestTests : XCTestCase
+
+@end
+
+@implementation ViewTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
